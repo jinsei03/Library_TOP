7a9514b20c794f190ca4b04e38f00fc49a079fa9
yami <qjasonc@gmail.com>
2024-11-30 01:03:46 -0800

    library allows user to add new books and lets them remove it

--- script.js
+++ script.js
@@ -15,25 +15,52 @@ function Book(title, author, pages, read) {
     }
 }
 
-const book1 = new Book("title-test", "auhtor-test", 100, true);
-const book2 = new Book("title-test2", "auhtor-test2", 1200, false);
+const cards = document.querySelector(".cards");
 
 function addBookToLibrary(book) {
-    return myLibrary.push(book);
+    myLibrary.push(book);
+    const card = document.createElement("li");
+    card.innerHTML = "<strong>Title:</strong> " + book.title + 
+    "<br><strong>Author:</strong> " + book.author +
+    "<br><strong>Pages:</strong> " + book.pages +
+    "<br><strong>Read:</strong> " + book.read; 
+    const remove = document.createElement("button");
+    remove.textContent = "Remove";
+    remove.addEventListener("click", () => {
+        const index = myLibrary.indexOf(book);
+        myLibrary.splice(index, 1);
+        cards.removeChild(card);
+    })
+    card.appendChild(remove);
+    cards.appendChild(card);
+    return console.table(myLibrary);
+
 }
 
-addBookToLibrary(book1);
-addBookToLibrary(book2);
+const dialog = document.querySelector("dialog");
+const title = document.querySelector("#title");
+const author = document.querySelector("#author");
+const pages = document.querySelector("#pages");
+const read = document.querySelector("#read");
+const confirm = document.querySelector("#confirm");
 
-const cards = document.querySelector(".cards");
-for(let i = 0; i < myLibrary.length; i++)
-    {
-        const card = document.createElement("li");
-        card.innerHTML = "<strong>Title:</strong> " + myLibrary[i].title + 
-        "<br><strong>Author:</strong> " + myLibrary[i].author +
-        "<br><strong>Pages:</strong> " + myLibrary[i].pages + 
-        "<br><strong>Read:</strong> " + myLibrary[i].read; 
-        cards.appendChild(card);
+const button = document.querySelector("button")
+button.addEventListener("click", () => {
+    dialog.show();
+})
+
+confirm.addEventListener("click", () => {
+    if(isOnlyWhitespace(title.value) == false && isOnlyWhitespace(author.value) == false && pages.value > 0)
+    {   
+        let newBook = new Book(title.value, author.value, pages.value, read);
+        addBookToLibrary(newBook);
     }
+    title.value = "";
+    author.value = "";
+    pages.value = "";
+})
 
+function isOnlyWhitespace(str) {
+    return str.trim() === "";
+  }
 
